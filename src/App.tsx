1e103e0f71ac35a736617339d5aef45e0e5b56cd
Katherinegf <110284567+Katherinegf@users.noreply.github.com>
2022-12-07 15:11:29 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import UserProvider from './contexts/UserContext';
+
+function App () {
+
+  return (
+
+<UserProvider>
+    <div className="container mt-5">
+
+    </div>
+
+</UserProvider>
+   
+  );
+}
+
+export default App
---
+++ src/components/UserList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {IUserContext, UserContext} from '../contexts/UserContext'
+
+const UserList = () => {
+  return (
+    <div>UserList</div>
+  )
+}
+
+export default UserList
